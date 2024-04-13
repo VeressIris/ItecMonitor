f07c1f8289515962b961c583a7d3ce2fa870f8f4
VeressIris <irisveress@gmail.com>
2024-04-14 01:52:59 +0300

    tiniest styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,1 @@
-<script>
-  import { onMount } from "svelte";
-  let test = "";
-  onMount(async () => {
-    const response = await fetch("http://127.0.0.1:3000");
-    test = await response.text();
-  });
-</script>
-
-<h1>Welcome to SvelteKit</h1>
-<p>
-  Go says: {test}
-</p>
+<h1>Home page</h1>
